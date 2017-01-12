d5cb98cb678fb05422f0ed7dabb1b927bddd4714
user <user@USER-PC>
2017-01-12 18:55:01 +0200

    name

--- Program/ChineseWhisperPlayer.cs
+++ Program/ChineseWhisperPlayer.cs
@@ -21,7 +21,7 @@ namespace CommunicationLab2
         //to remove
         int cMinListeningTCPPort = 6001;
         int cMaxListeningTCPPort = 7000;
-        string _programName = "L&I_Networking17";
+        string _programName = "IsisNetworking17";
         IPAddress localAddr = GetLocalIPAddress();
 
         short _meAsServerListeningPort;
